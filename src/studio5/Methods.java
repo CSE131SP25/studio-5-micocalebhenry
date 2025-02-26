b62881e7c9c236b77ce8d9946f3fa00ee7887882
Caleb <Caleb@localhost>
2025-02-26 15:22:19 -0600

    bulls eye

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -28,23 +28,29 @@ public class Methods {
 	 * @param radius the radius of the bull's eye
 	 */
 	public static void drawBullsEye(double x, double y, double radius) {
+		
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.filledCircle(x, y, radius);
+		
 
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
 
-		
+		StdDraw.setPenColor(0, 109, 219);
+		StdDraw.filledCircle(x, y, (3.0/4.0)*radius);
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
 
-		
+		StdDraw.setPenColor(146, 0, 0);
+		StdDraw.filledCircle(x, y, (2.0/4.0)*radius);
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		
+		StdDraw.setPenColor(255, 255, 109);
+		StdDraw.filledCircle(x, y, (1.0/4.0)*radius);
 		
 	}
 
